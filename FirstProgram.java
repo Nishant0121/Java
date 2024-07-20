ddcdbb8962f4e77cf6622cb4d5b83f1af19375a7
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-07-20 14:31:34 +0530

    fist change

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,7 +1,9 @@
 public class FirstProgram{
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Name : Nishant Anant Patil");
+        System.out.println("Dipartment : IT");
+        System.out.println("Roll No : 99");
 
     }
 }
